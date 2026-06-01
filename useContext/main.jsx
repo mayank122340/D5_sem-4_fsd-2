852ef7a26fc63047ee5b89c08f188248a9a7f1e3
mayank122340 <118726179+mayank122340@users.noreply.github.com>
2026-06-01 09:26:12 +0530

    Add files via upload

---
+++ useContext/main.jsx
@@ -0,0 +1,11 @@
+import React,{createContext} from "react";
+import Comp from './Comp';
+const Fname=createContext();
+function Main(){
+    return(
+        {
+            <Fname.Provider value="Ram"><Comp/></Comp></Fname.Provider>
+        }
+    );
+}export default Main;
+export{Fname}
